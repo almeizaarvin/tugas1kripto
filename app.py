d175d5eeee3b59c912b421f8269193d3d5b76a9a
almeizaarvin <13519066@std.stei.itb.ac.id>
2023-02-02 13:00:56 +0700

    Add file upload and crypto method

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, static_folder="static")
 
 
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        method = request.form['method']
+        if file:
+            # Read the contents of the file
+            file_contents = file.read().decode('utf-8')
+            return render_template('index.html', text=file_contents, method=method)
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
